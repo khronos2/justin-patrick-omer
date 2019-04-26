b1ce4344160dab664ca36ef4eaffa852e9c161eb
root <root@localhost.localdomain>
2019-04-26 00:03:57 -0400

    Initial script creation

---
+++ LinuxEnumeration.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#Linux Enumeration Script
+#Authors: Justin Bunker, Patrick Walker, and Omer Turhan
+#Description: The purpose of this script is to conduct basic enumeration and provide advice regarding the information found.
+
